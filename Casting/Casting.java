1125c7044e4d1244a5e65991c0873399b1676bfc
edrobi <ekdrobinski@gmail.com>
2023-05-08 13:19:12 -0500

    spaced better

--- Casting/Casting.java
+++ Casting/Casting.java
@@ -2,7 +2,7 @@ public class Casting {
  public static void main(String[] args) {
 
         double bigDouble = 112.35;
-        System.out.println("Double:"+ bigDouble);
+        System.out.println("Double: "+ bigDouble);
         
         int bigInt =(int)bigDouble;
         System.out.println("Int: " + bigInt);
@@ -11,7 +11,7 @@ public class Casting {
         System.out.println("The string value is: "  + Integer.parseInt(numString));
         
         int sameInt = 49;
-        System.out.println("The int value is:" + bigInt);
+        System.out.println("The int value is: " + bigInt);
 
    }
 }
